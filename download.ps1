257afcf899e4b2c3e5424aaabf25353224771541
Caleb <ande4905@gmail.com>
2020-01-03 17:40:37 -0600

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -1,7 +1,8 @@
 Import-Module BitsTransfer
 #$url = "https://raw.githubusercontent.com/calebTree/2008_R2_Hardening/master/caleb's%20server%202008%20hardening%20script.ps1?token=AAUO6ZQHAMEJXAFDI4FXM726B7ES4"
 #$url = "https://bit.ly/39EmNKI"
-$url = "https://bit.ly/36pOxB0"
+#$url = "https://bit.ly/36pOxB0"
+$url = "https://bit.ly/2Qnjznf"
 $output = "$env:userprofile\desktop\W08R2Harden.ps1"
 Start-BitsTransfer -Source $url -Destination $output
 . $env:userprofile\desktop\W08R2Harden.ps1
