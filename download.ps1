7291eaec850abc55c6e1b850654d5cc1766dbe91
Caleb <ande4905@gmail.com>
2020-01-03 18:47:39 -0600

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -1,6 +1,5 @@
 Import-Module BitsTransfer
-#$url = "https://bit.ly/36mYlvB"
-$url = "https://bit.ly/2sNP2Ga"
+#Private Main Script $url = "https://bit.ly/36mYlvB"
 $output = "$env:userprofile\desktop\W08R2Harden.ps1"
 Start-BitsTransfer -Source $url -Destination $output
 . $env:userprofile\desktop\W08R2Harden.ps1
