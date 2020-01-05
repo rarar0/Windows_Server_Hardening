1287550d7b1bba515274e8fae6dbd6e0cdd6764f
Caleb <ande4905@gmail.com>
2020-01-04 20:56:04 -0600

    fixing color

--- caleb's server 2008 hardening script.ps1
+++ caleb's server 2008 hardening script.ps1
@@ -87,7 +87,7 @@ function disableTeredo{
 $host.UI.RawUI.foregroundcolor = "green"
 Write-Host "`nDisabling Teredo"
 $host.UI.RawUI.foregroundcolor = "cyan"
-cmd /K 'echo > script.vbs set shell = CreateObject("WScript.Shell"):shell.SendKeys "netsh{ENTER}interface{ENTER}teredo{ENTER}set state disabled{ENTER}exit{ENTER}" & script.vbs'
+cmd /c 'echo > script.vbs set shell = CreateObject("WScript.Shell"):shell.SendKeys "netsh{ENTER}interface{ENTER}teredo{ENTER}set state disabled{ENTER}exit{ENTER}" & script.vbs & cmd /c'
 $host.UI.RawUI.foregroundcolor = "white"
 }
 # --------- disable administrative shares via registry ---------
