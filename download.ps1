5712d04cb8fcb5f3309b0244b9cd8ff5748e0de7
Caleb <ande4905@gmail.com>
2020-01-16 23:25:36 -0600

    password complex fix

--- download.ps1
+++
@@ -1,5 +0,0 @@
-Import-Module BitsTransfer
-#Private Main Script $url = "https://bit.ly/36mYlvB"
-$output = "$env:userprofile\desktop\W08R2Harden.ps1"
-Start-BitsTransfer -Source $url -Destination $output
-. $env:userprofile\desktop\W08R2Harden.ps1
