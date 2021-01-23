b2e09ebcb82786ac53397738c89faf189519b253
rarar0/0 <boombot101@gmail.com>
2021-01-23 09:24:06 -0600

    Update hardening_exe.ps1

--- hardening_exe.ps1
+++ hardening_exe.ps1
@@ -55,7 +55,8 @@ Param(
     [switch]$timeStamp,
     [switch]$processes,
     [switch]$netcease,
-    [switch]$downloadlist
+    [switch]$downloadlist,
+    [switch]$GPAudit
 )
 #region misc
 # --------- create output directory on desktop ---------
@@ -366,7 +367,7 @@ https://www.catalog.update.microsoft.com/search.aspx?q=kb4012215
 https://www.catalog.update.microsoft.com/Search.aspx?q=security%20monthly%20quality%20rollup%20for%20Windows%20Server%202008%20r2%20for%20x64
 https://gallery.technet.microsoft.com/scriptcenter/Kerberos-Golden-Ticket-b4814285
 Upgrading PowerShell for W2K8
-Run “Windows PowerShell Modules” (Admin Tools / Start Menu)
+Run Windows PowerShell Modules (Admin Tools / Start Menu)
 •	Install .NET Framework 4.0 or .NET Framework 4.5
 o	https://www.microsoft.com/en-us/download/details.aspx?id=17851
 o	https://www.microsoft.com/en-us/download/details.aspx?id=30653
@@ -377,51 +378,46 @@ o	https://www.microsoft.com/en-us/download/details.aspx?id=34595
 o	https://www.microsoft.com/en-us/download/details.aspx?id=40855
 Reboot a few times...cross your fingers
 https://www.glasswire.com/
-
-=resetgrouppolicy.bat========================
-RD /S /Q "%WinDir%\System32\GroupPolicyUsers"
-RD /S /Q "%WinDir%\System32\GroupPolicy"
-dcgpofix /target:both
-gpupdate /force /logoff
-=============================================
--GPO.ps1-------------------------------------
-auditpol /set /category:"Account Logon" /subcategory:"Audit Credential Validation" /failure:enable /success:disable
-auditpol /set /category:"Account Logon" /subcategory:"Audit Other Account Logon Events" /failure:enable /success:enable
-
-auditpol /set /category:"Account Management" /subcategory:"Audit Other Account Management Events Properties" /failure:enable /success:enable
-auditpol /set /category:"Account Management" /subcategory:"Audit Security Group Management Properties" /failure:enable /success:enable
-auditpol /set /category:"Account Management" /subcategory:"Audit User Account Management Properties" /failure:enable /success:enable
-
-auditpol /set /category:"Logon/Logoff" /subcategory:"Audit Account Lockout Properties" /failure:enable /success:enable
-auditpol /set /category:"Logon/Logoff" /subcategory:"Audit Logoff Properties" /failure:enable /success:enable
-auditpol /set /category:"Logon/Logoff" /subcategory:"Audit Other Logon/Logoff Events" /failure:enable /success:enable
-auditpol /set /category:"Logon/Logoff" /subcategory:"Audit Special Logon" /failure:enable /success:enable
-
-auditpol /set /category:"Object Access" /subcategory:"Audit Kernel Object" /failure:enable /success:enable
-auditpol /set /category:"Object Access" /subcategory:"Audit Other Object Access Events" /failure:enable /success:enable
-auditpol /set /category:"Object Access" /subcategory:"Audit Registry" /failure:enable /success:enable
-auditpol /set /category:"Object Access" /subcategory:"Audit SAM" /failure:enable /success:disable
-
-auditpol /set /category:"Policy Change" /subcategory:"Audit Audit Policy Change" /failure:enable /success:enable
-auditpol /set /category:"Policy Change" /subcategory:"Audit Authentication Policy" /failure:enable /success:enable
-auditpol /set /category:"Policy Change" /subcategory:"Audit Authorization Policy Change" /failure:enable /success:enable
-auditpol /set /category:"Policy Change" /subcategory:"Audit Other Policy Change Events" /failure:enable /success:enable
-
-auditpol /set /category:"Privilege Use" /subcategory:"Audit Non-Sensitive Privilege use" /failure:enable /success:enable
-auditpol /set /category:"Privilege Use" /subcategory:"Audit Other Privilege use events" /failure:enable /success:enable
-auditpol /set /category:"Privilege Use" /subcategory:"Audit sensitive Privilege use" /failure:enable /success:enable
-
-auditpol /set /category:"System" /subcategory:"Audit Other System Events" /failure:enable /success:enable
-auditpol /set /category:"System" /subcategory:"Audit Security System Extension" /failure:enable /success:enable
-auditpol /set /category:"System" /subcategory:"Audit System Integrity" /failure:enable /success:disable
------------------------------------------------
-
 dafodor39810204
 FatAlbert42!
 "
 }    
 if($downloadlist){
     downloadlist
+}
+function GPAudit{
+    auditpol /set /category:"Account Logon" /subcategory:"Audit Credential Validation" /failure:enable /success:disable
+    auditpol /set /category:"Account Logon" /subcategory:"Audit Other Account Logon Events" /failure:enable /success:enable
+
+    auditpol /set /category:"Account Management" /subcategory:"Audit Other Account Management Events Properties" /failure:enable /success:enable
+    auditpol /set /category:"Account Management" /subcategory:"Audit Security Group Management Properties" /failure:enable /success:enable
+    auditpol /set /category:"Account Management" /subcategory:"Audit User Account Management Properties" /failure:enable /success:enable
+
+    auditpol /set /category:"Logon/Logoff" /subcategory:"Audit Account Lockout Properties" /failure:enable /success:enable
+    auditpol /set /category:"Logon/Logoff" /subcategory:"Audit Logoff Properties" /failure:enable /success:enable
+    auditpol /set /category:"Logon/Logoff" /subcategory:"Audit Other Logon/Logoff Events" /failure:enable /success:enable
+    auditpol /set /category:"Logon/Logoff" /subcategory:"Audit Special Logon" /failure:enable /success:enable
+
+    auditpol /set /category:"Object Access" /subcategory:"Audit Kernel Object" /failure:enable /success:enable
+    auditpol /set /category:"Object Access" /subcategory:"Audit Other Object Access Events" /failure:enable /success:enable
+    auditpol /set /category:"Object Access" /subcategory:"Audit Registry" /failure:enable /success:enable
+    auditpol /set /category:"Object Access" /subcategory:"Audit SAM" /failure:enable /success:disable
+
+    auditpol /set /category:"Policy Change" /subcategory:"Audit Audit Policy Change" /failure:enable /success:enable
+    auditpol /set /category:"Policy Change" /subcategory:"Audit Authentication Policy" /failure:enable /success:enable
+    auditpol /set /category:"Policy Change" /subcategory:"Audit Authorization Policy Change" /failure:enable /success:enable
+    auditpol /set /category:"Policy Change" /subcategory:"Audit Other Policy Change Events" /failure:enable /success:enable
+
+    auditpol /set /category:"Privilege Use" /subcategory:"Audit Non-Sensitive Privilege use" /failure:enable /success:enable
+    auditpol /set /category:"Privilege Use" /subcategory:"Audit Other Privilege use events" /failure:enable /success:enable
+    auditpol /set /category:"Privilege Use" /subcategory:"Audit sensitive Privilege use" /failure:enable /success:enable
+    
+    auditpol /set /category:"System" /subcategory:"Audit Other System Events" /failure:enable /success:enable
+    auditpol /set /category:"System" /subcategory:"Audit Security System Extension" /failure:enable /success:enable
+    auditpol /set /category:"System" /subcategory:"Audit System Integrity" /failure:enable /success:disable
+}
+if(GPAudit){
+    GPAudit
 }
 # --------- group policy tool ---------
 function GPTool{
@@ -2650,7 +2646,8 @@ function avail {
     timeStamp (timestamp Script_Output)
     getTools (download and install your tools)
     pickAKB (Provides applicable KB info then prompts for KB and downloads <KB>.msu to `"downloads`")
-    GPTool (opens GP info tool)"
+    GPTool (opens GP info tool)
+    GPAudit (Enables GP Logging)"
     Write-Host -ForegroundColor Gray -BackgroundColor DarkCyan "`n------- Extra: -------
     loopPing (ping all IP addresses in a class C network)
     ports (displays common ports file)
